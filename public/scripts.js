f9b84d42c7c90eb90050a450e626fb1ddb492dcf
Julius <julius.simonelli@gmail.com>
2023-07-07 18:42:33 -0700

    trying to add leaderboard

--- public/scripts.js
+++ public/scripts.js
@@ -1,6 +1,8 @@
 const quizContainer = document.getElementById('quiz-container');
 const nextButton = document.getElementById('next-button');
 const resultsContainer = document.getElementById('results-container');
+// Firebase Firestore initialization
+const db = firebase.firestore();
 let currentQuestionIndex = 0;
 let questions = [];
 let score = 0;
@@ -48,6 +50,15 @@ function submitAnswer() {
   } else {
     brierScore += Math.pow(0 - userConfidence, 2);
   }
+  // Save user's answer
+  const username = document.getElementById('username').value.trim();
+  db.collection('answers').add({
+    username,
+    questionIndex: currentQuestionIndex,
+    userAnswer,
+    correctAnswer,
+    userConfidence,
+    timestamp: firebase.firestore.FieldValue.serverTimestamp(),
 }
 
 function displayResults() {
@@ -64,5 +75,58 @@ function displayResults() {
 
   resultsContainer.style.display = 'block';
 }
+
+function calculateScores() {
+  return db.collection('answers').get()
+    .then(snapshot => {
+      const answers = snapshot.docs.map(doc => doc.data());
+      const users = {};
+
+      // Group answers by user
+      answers.forEach(answer => {
+        if (!(answer.username in users)) {
+          users[answer.username] = [];
+        }
+        users[answer.username].push(answer);
+      });
+
+      // Calculate each user's score
+      const scores = {};
+      for (const username in users) {
+        let score = 0;
+        let brierScore = 0;
+        users[username].forEach(answer => {
+          const userConfidence = answer.userConfidence;
+          if (answer.userAnswer === answer.correctAnswer) {
+            score++;
+            brierScore += Math.pow(1 - userConfidence, 2);
+          } else {
+            brierScore += Math.pow(0 - userConfidence, 2);
+          }
+        });
+        scores[username] = {
+          score,
+          brierScore: brierScore / users[username].length,
+        };
+      }
+
+      return scores;
+    });
+}
+
+function displayLeaderboard(scores) {
+  const leaderboardContainer = document.getElementById('leaderboard-container');
+  leaderboardContainer.innerHTML = `
+    <h2>Leaderboard</h2>
+    ${Object.entries(scores).map(([username, {score, brierScore}]) => `
+      <p>${username}: ${score} correct, Brier score ${brierScore.toFixed(2)}</p>
+    `).join('')}
+  `;
+  leaderboardContainer.style.display = 'block';
+}
+
+// Display the leaderboard after the results
+calculateScores().then(displayLeaderboard);
+
 
 loadQuestions();
