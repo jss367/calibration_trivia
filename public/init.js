5573ee10f8d7669f37d086410104ecbff37e61ee
Julius <julius.simonelli@gmail.com>
2023-07-07 19:51:00 -0700

    Update init.js

--- public/init.js
+++ public/init.js
@@ -6,6 +6,10 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+
+// The docs say I can leave the apiKey here:
+// Usually, you need to fastidiously guard API keys (for example, by using a vault service or setting the keys as environment variables); however, API keys for Firebase services are ok to include in code or checked-in config files.
+// https://firebase.google.com/docs/projects/api-keys
 const firebaseConfig = {
   apiKey: "AIzaSyDTFpB2roR1dV2xDVNDpig6ykuco5iNYCU",
   authDomain: "calibration-trivia.firebaseapp.com",
