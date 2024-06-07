6f9c62eb05e6587e7e41f1d988bc19639ea205a8
Julius Simonelli <julius.simonelli@gmail.com>
2024-06-06 20:42:33 -0700

    wip

---
+++ public/dist/init.js
@@ -0,0 +1,8 @@
+'use strict';
+
+var _firebaseConfig = require('./firebaseConfig');
+
+// Any additional initialization or setup can be done here
+console.log('Firebase initialized:', _firebaseConfig.app.name); // src/init.js
+
+// Import Firebase services from the configuration file
---
+++ src/init.js
@@ -0,0 +1,7 @@
+// src/init.js
+
+// Import Firebase services from the configuration file
+import { app } from './firebaseConfig';
+
+// Any additional initialization or setup can be done here
+console.log('Firebase initialized:', app.name);
